a3c3d130474c668a1601be5b32cf363b79c15b3a
Carson <rodriguescarson@hotmail.com>
2021-09-15 23:01:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useState } from "react";
 
-function App() {
+const bookDB = {
+  Motivation: [
+    {
+      name: "Subtle Art Of not giving a fu*k",
+      rating: "5/5"
+    },
+    {
+      name: "48 Laws Of Power",
+      rating: "4/5"
+    }
+  ],
+  business: [
+    {
+      name: "Rich dad poor dad",
+      rating: "5/5"
+    },
+    {
+      name: "Zero to One",
+      rating: "5/5"
+    }
+  ],
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("business");
+
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> GoodBooks </h1>
+      <p style={{ fontSize: "smaller" }}>
+        Checkout my favorite books. Select a genre to get started
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {bookDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
