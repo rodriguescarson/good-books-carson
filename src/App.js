84e2d1b54b8d569308ad8739f7b538ded7792b95
Carson <rodriguescarson@hotmail.com>
2021-09-28 23:42:14 +0530

    Added: new books

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ const bookDB = {
     {
       name: "48 Laws Of Power",
       rating: "4/5"
-    }
+    },
+
+    {
+      name: "Think like a monk",
+      rating: "5/5"
+    },
   ],
   business: [
     {
@@ -20,11 +25,16 @@ const bookDB = {
     {
       name: "Zero to One",
       rating: "5/5"
+    },
+    {
+      name: "Lean Startup",
+      rating: "4.5/5"
     }
   ],
   javascript: [
     { name: "Eloquent JavaScript", rating: "4/5" },
-    { name: "You Don't Know JS", rating: "3.5/5" }
+    { name: "You Don't Know JS", rating: "3.5/5" },
+    { name: "Core Javascript", rating: "5/5" }
   ]
 };
 
@@ -37,68 +47,68 @@ export default function App() {
 
   return (
     <div className="App">
-    <header>
-    <h1  className="header">Good Books</h1>
-    <p style={{ fontSize: "smaller" }}>
-        Checkout my favorite books. Select a genre to get started
-      </p>
+      <header>
+        <h1 className="header">Good Books</h1>
+        <p style={{ fontSize: "smaller" }}>
+          Checkout my favorite books. Select a genre to get started
+        </p>
       </header>
 
-<main>
-      <div>
-        {Object.keys(bookDB).map((genre) => (
-          <button
-            onClick={() => genreClickHandler(genre)}
-            className="btn-check"
-            key={genre}
-          >
-            {genre}
-          </button>
-        ))}
-      </div>
-        <hr style={{ width: "80%",marginLeft:"10%"}}/>
-      <div>
-        <ul>
-          {bookDB[selectedGenre].map((book) => (
-            <li
-              key={book.name}
-              style={{
-                listStyle: "none",
-                padding: "1rem",
-                width: "100%",
-                margin: "1rem 0rem",
-                borderRadius: "0.5rem"
-              }}
+      <main>
+        <div>
+          {Object.keys(bookDB).map((genre) => (
+            <button
+              onClick={() => genreClickHandler(genre)}
+              className="btn-check"
+              key={genre}
             >
-              {" "}
-              <div style={{ fontSize: "larger",textAlign: "center" }}> {book.name} </div>
-              <div style={{ fontSize: "smaller",textAlign: "center" }}> {book.rating} </div>
-            </li>
+              {genre}
+            </button>
           ))}
-        </ul>
-      </div>
+        </div>
+        <hr style={{ width: "80%", marginLeft: "10%" }} />
+        <div>
+          <ul>
+            {bookDB[selectedGenre].map((book) => (
+              <li
+                key={book.name}
+                style={{
+                  listStyle: "none",
+                  padding: "1rem",
+                  width: "100%",
+                  margin: "1rem 0rem",
+                  borderRadius: "0.5rem"
+                }}
+              >
+                {" "}
+                <div style={{ fontSize: "larger", textAlign: "center" }}> {book.name} </div>
+                <div style={{ fontSize: "smaller", textAlign: "center" }}> {book.rating} </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
 
       <footer className="footer container">
-    <div className="footer-header">Connect with me on social media</div>
-    <ul className="social-links list-non-bullet">
-        <li className="list-item-inline">
+        <div className="footer-header">Connect with me on social media</div>
+        <ul className="social-links list-non-bullet">
+          <li className="list-item-inline">
             <a className="link" href="https://www.linkedin.com/in/rodriguescarson">
-                Linkedin
+              Linkedin
             </a>
-        </li>
-        <li className="list-item-inline">
+          </li>
+          <li className="list-item-inline">
             <a className="link" href="https://github.com/rodriguescarson">
-                Github
+              Github
             </a>
-        </li>
-        <li className="list-item-inline">
+          </li>
+          <li className="list-item-inline">
             <a className="link" href="https://twitter.com/carsonmarz">
-                twitter
+              twitter
             </a>
-        </li>
-    </ul>
-</footer>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
